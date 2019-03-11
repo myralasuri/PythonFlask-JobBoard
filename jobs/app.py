08b44bda49c5d2ec3815c5782db950da4f8a1c9c
Suryanarayana Myrala <suryanarayana.myrala@factset.com>
2019-03-11 19:35:45 +0530

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,39 @@
-from flask import Flask
-import sys
+import sqlite3
+from flask import Flask,g
 from flask import render_template
 app = Flask(__name__)
+
+PATH = '../db/jobs.sqlite'
+def open_connection():
+    connection =None
+    connection = getattr(g._connection, default=None)
+    if connection is None :
+        connection, g._connection = sqlite3.connect(PATH)
+        connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values = (), commit =False, single =False):
+    connection = open_connection()
+    cursor =connection.execute(sql, values)
+    results =None
+    if commit :
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+@app.teardown_appcontext
+def close_connection(exception ):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
+
+
+
+
+
+
 
 @app.route('/')
 @app.route('/jobs',methods = ['get'])
